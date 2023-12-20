ca95598fff8b86ebd19ba67994d23e1e0729646c
DipesBartaula <dipeshbartaula4@gmail.com>
2023-12-20 22:21:09 +0545

    register user and hash credential

--- app.js
+++ app.js
@@ -1,17 +1,49 @@
 const express = require("express");
 const app = express();
 const { connectDatabase } = require("./database/database");
+const User = require("./model/userModel.js");
+
+//for hashing
+const bcrypt = require("bcryptjs");
+
+//Node will use DOTENV
+require("dotenv").config();
+
+//telling to parse data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Connecting to database
-connectDatabase();
+connectDatabase(process.env.MONGO_URI);
 
+// testing api
 app.get("/", (req, res) => {
   return res.status(200).json({
     message: "Nice going",
   });
 });
 
+//registering user
+app.post("/register", async (req, res) => {
+  const { email, name, phoneNumber, password } = req.body;
+  if (!email || !name || !phoneNumber || !password) {
+    return res.status(401).json({
+      message: "Provide email,name,phoneNumber,password",
+    });
+  }
+  await User.create({
+    userEmail: email,
+    userName: name,
+    userPhoneNumber: phoneNumber,
+    userPassword: bcrypt.hashSync(password, 8),
+  });
+  return res.status(200).json({
+    message: "User registered successfully",
+  });
+});
+
+const port = process.env.PORT;
 //Listen server
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server has started at port 3000");
 });
--- database/database.js
+++ database/database.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 //connecting to database
-exports.connectDatabase = async () => {
-  await mongoose.connect(
-    "mongodb+srv://e-Procure:e-Procure@cluster0.fczqyfy.mongodb.net/?retryWrites=true&w=majority"
-  );
+exports.connectDatabase = async (URI) => {
+  await mongoose.connect(URI);
   console.log("Database connected successfully");
 };
